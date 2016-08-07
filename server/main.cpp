49c1520acf7e87254661811936b279573b28bf1a
Ivgeni Slabkovski <zhenya@zhenya.ca>
2016-08-07 15:18:37 -0400

    Changes responsibility of command line option parsing to boost::program_options.

--- server/main.cpp
+++ server/main.cpp
@@ -2,36 +2,83 @@
 // main.cpp
 // ~~~~~~~~
 //
-// Copyright (c) 2003-2015 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+// Copyright (c) 2016 Ivgeni Slabkovski 
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
+#include <cstdlib>
 #include <iostream>
 #include <string>
 #include <boost/asio.hpp>
+#include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
 #include "server.hpp"
 
+
 int main(int argc, const char* argv[]) {
+  using namespace http::server;
+  using namespace boost::program_options;
+  using namespace boost::filesystem;
+  using namespace std;
+
   try {
     // Check command line arguments.
-    if (argc != 4) {
-      std::cerr << "Usage: http_server <address> <port> <doc_root>\n";
-      std::cerr << "  For IPv4, try:\n";
-      std::cerr << "    receiver 0.0.0.0 80 .\n";
-      std::cerr << "  For IPv6, try:\n";
-      std::cerr << "    receiver 0::0 80 .\n";
-      return 1;
-    }
+    options_description info, config, cl_options;
+    
+    info.add_options()
+        ("version,v", value<bool>()->implicit_value(true)->notifier(
+                [&](bool) {
+                  cerr << "0" << endl;
+                  exit(0);
+                }
+            ),
+            "Print version string and exit.")
+        ("help,?", value<bool>()->implicit_value(true)->notifier(
+                [&](bool) {
+                  options_description help("Usage: http_server <host> <port> <application_root>");
+                  help.add(info).add(config);
+                  cerr << "new spot\n" << help;
+                  exit(0);
+                }
+            ),
+            "Display help information and exit.")
+        ;
+    
+    string host, port, application_root;
+    
+    config.add_options()
+        ("host,h", value<string>(&host)->default_value("0.0.0.0"),
+            "The IP address of the host the server is running on.")
+        ("port,p", value<string>(&port)->default_value("3000"),
+            "The port the server will bind to.")
+        ("app-root,r", value<string>(&application_root)->default_value("./"),
+            "Application root directory.")
+        ;
+    
+    cl_options.add(info).add(config);
+    
+    variables_map cl_vars;
+    
+    auto cl_parser = command_line_parser(argc, argv).options(cl_options);
+    
+    store(cl_parser.run(), cl_vars);
+    notify(cl_vars);
+    
+    path app_root(application_root);
+    application_root = canonical(app_root).string();
+    
+    cerr << "[INFO]: initializing http_server[" << host << ":" << port << ":"
+         << application_root << "]" << endl;
 
-    // Initialise the server.
-    http::server::server s(argv[1], argv[2], argv[3]);
+    // Initialize the server.
+    server http_server(host, port, application_root);
 
     // Run the server until stopped.
-    s.run();
-  } catch (std::exception& e) {
-    std::cerr << "exception: " << e.what() << "\n";
+    http_server.run();
+  } catch (exception& error) {
+    cerr << "error: " << error.what() << "\n";
     return 1;
   }
 }
